8a7cdee195c23eab20c435211b9fe915e1ec8331
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-12-19 19:17:46 -0300

    Created bat file to run wsgi django server

---
+++ run_waitress.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Script que será executado pelo NSSM
+
+REM Ativação e arranque (mantenha o caminho completo, pois o NSSM pode não saber onde está)
+call "D:\devdj\system-store\.venv\Scripts\activate.bat"
+
+REM Arranque do Waitress
+waitress-serve --port=8000 system_store.wsgi:application
+
+REM Não coloque 'pause' ou 'deactivate' no final, pois o serviço deve correr até ser parado.
